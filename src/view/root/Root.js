3d094b14425522a07f340c9b71845497ca18d1e0
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2022-05-07 19:22:32 +0200

    adding the first component with the Styled components method

---
+++ src/view/root/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../../components/button/Button';
+
+function Root() {
+  return (
+    <>
+      <h1> jestem </h1>
+      <Button>byton</Button>
+    </>
+  );
+}
+
+export default Root;
