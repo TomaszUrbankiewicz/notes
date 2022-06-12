1a86f7fa6f2524cc0928bdbe320a8cd22c08104d
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2022-06-12 20:40:31 +0200

    adding icons to the Card component

--- src/components/molecules/cards/Card.js
+++ src/components/molecules/cards/Card.js
@@ -4,6 +4,7 @@ import PropType from "prop-types";
 import Button from "../../atoms/button/Button";
 import Heading from "../../atoms/heading/Heading";
 import Paragraph from "../../atoms/paragraph/Paragraph";
+import link from '../../../assets/link.svg';
 
 const StyledWrapper = styled.div`
     min-height: 380px;
@@ -17,6 +18,7 @@ const StyledWrapper = styled.div`
 const InnerWrapper = styled.div`
     padding:17px 30px; 
     background-color: ${({activeColor,theme}) => (activeColor ? theme[activeColor] : 'white')};
+    position: relative;
 `;
 const InnerWrapper2 = styled.div`
     padding:17px 30px; 
@@ -31,6 +33,25 @@ const DataInfo = styled(Paragraph)`
     font-weight: 600;
     font-size: 14px;
 `
+const StyledAvatar = styled.img`
+    width: 86px;
+    height: 86px;
+    border: 5px solid ${({theme}) => theme.twitter};
+    border-radius: 50%;
+    position: absolute;
+    right: 25px;
+    top:35px;
+    z-index: 2;
+`
+const StyledLink = styled.a`
+    width: 75px;
+    height: 75px;
+    border-radius: 50%;
+    background:white url(${link});
+    position: absolute;
+    right: 32px;
+    top:20px;
+`
 
 const Card = ({cardType}) => {
     return(
@@ -38,6 +59,8 @@ const Card = ({cardType}) => {
             <InnerWrapper activeColor={cardType} >
                 <Heading big>Tomek Urbankiewicz</Heading>
                 <DataInfo>12.06.2022</DataInfo>
+                {cardType==='twitter' && <StyledAvatar src="https://zaira.pl/userdata/public/news/images/177.jpg" />}
+                {cardType==='article' && <StyledLink href="https://github.com/TomaszUrbankiewicz" />}
             </InnerWrapper>
             <InnerWrapper2 flex>
                 <Paragraph>Testowy paragraw, w projekcie gdzie używam styled components.</Paragraph>
